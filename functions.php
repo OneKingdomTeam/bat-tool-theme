47297017fc3ccc3533bccef41860c5edb6e20cc8
VrubPetGit <petr@vrubel.online>
2023-10-30 15:01:42 +0100

    redownloaded version of the plugin

--- functions.php
+++ functions.php
@@ -4,6 +4,6 @@ add_action('wp_enqueue_scripts', 'ttt_bulma_enqueue_bulma' );
 
 function ttt_bulma_enqueue_bulma(){
 
-    wp_enqueue_style( 'ttt_bulma_css', plugin_dir_url( __FILE__ ) . 'css/bulma.min.css' );
-
+    wp_enqueue_style( 'ttt_bulma_css', get_stylesheet_directory_uri() . '/css/bulma.min.css' );
+    wp_enqueue_style( 'thet_local_styles', get_stylesheet_directory_uri() . '/style.css' );
 }
